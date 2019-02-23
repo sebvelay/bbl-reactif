3e66360f6fa7ca1c1e1365b33fb3f4f2e5e5d2d0
Sebastien Velay <sebastien.velay@gmail.com>
2019-02-23 17:02:21 +0100

    feat(Mono) : prepare for merge

--- src/main/java/com/svelay/demo/service/DemoService.java
+++ src/main/java/com/svelay/demo/service/DemoService.java
@@ -3,6 +3,7 @@ package com.svelay.demo.service;
 import com.svelay.demo.pojo.Message;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Component
 public class DemoService {
@@ -17,5 +18,14 @@ public class DemoService {
                         .index(i++)
                         .build()));
     }
+
+    public Mono<String> getMessage() {
+        return Mono.just("Quel est la réponse à la terre, l'univers et le reste ?");
+    }
+
+    public Mono<Integer> getId() {
+        return Mono.just(42);
+    }
+
 
 }
