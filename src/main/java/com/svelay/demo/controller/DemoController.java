3c9eada979c86fe06683ff5a9631b43179552b5f
Sebastien Velay <sebastien.velay@gmail.com>
2019-02-23 10:52:01 +0100

    feat(webflux) : change to webflux app

--- src/main/java/com/svelay/demo/controller/DemoController.java
+++ src/main/java/com/svelay/demo/controller/DemoController.java
@@ -2,13 +2,14 @@ package com.svelay.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class DemoController {
 
     @GetMapping
-    public String aSimpleController() {
-        return "hello les Z";
+    public Mono<String> aSimpleController() {
+        return Mono.just("hello les Z");
     }
 
 }
