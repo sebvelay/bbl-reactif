d897e6923454d05b1e8accde85ce0b17b609a81f
Sebastien Velay <sebastien.velay@gmail.com>
2019-02-23 15:35:20 +0100

    feat(flux) : controller for inifinite stream

--- src/main/java/com/svelay/demo/controller/DemoController.java
+++ src/main/java/com/svelay/demo/controller/DemoController.java
@@ -1,9 +1,14 @@
 package com.svelay.demo.controller;
 
+import com.svelay.demo.pojo.Message;
 import com.svelay.demo.service.DemoService;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.time.Duration;
 
 @RestController
 public class DemoController {
@@ -17,6 +22,13 @@ public class DemoController {
     @GetMapping
     public Mono<String> aSimpleController() {
         return Mono.just("hello les Z");
+    }
+
+    @GetMapping(value = "/infinite", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
+    public Flux<Message> getInfinite(){
+
+        return demoService.generateInfinteFlux()
+                .delayElements(Duration.ofSeconds(1));
     }
 
 }
